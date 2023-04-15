aea800dbf64b51cdffecd361716b444dbb7895c3
ManuelR11 <rod21509@uvg.edu.gt>
2023-04-14 18:49:07 -0600

    Pagina web inicio

---
+++ src/START/index.js
@@ -0,0 +1,10 @@
+const myButtonContainer = document.getElementById("myButtonContainer");
+const myButton = document.getElementById("myButton");
+const myButtonContainer_1 = document.getElementById("myButtonContainer");
+const myButton_ = document.getElementById("myButton");
+const myTitle_1 = document.getElementById("myTitle");
+const myParagraph = document.getElementById("myParagraph");
+
+const validarLogin = () => {
+    alert('Teste');
+  }
