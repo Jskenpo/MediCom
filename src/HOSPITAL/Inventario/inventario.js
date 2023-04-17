ff2a54eee83493c27bba68b08f3df7ebcedee5e7
ManuelR11 <rod21509@uvg.edu.gt>
2023-04-16 21:39:35 -0600

    Search en listas

--- src/HOSPITAL/Inventario/inventario.js
+++ src/HOSPITAL/Inventario/inventario.js
@@ -1,4 +1,85 @@
+const myTable = document.createElement('table');
+myTable.id = 'TableInventario';
+
+const searchInput = document.createElement('input');
+searchInput.id = 'searchInput';
+
+const searchContainer = document.querySelector('.search__container');
+searchContainer.appendChild(searchInput);
+
+document.body.appendChild(myTable);
+
+fetch('http://3.22.74.121:3000/Inventario')
+  .then(response => response.json())
+  .then(data => {
+    myTable.innerHTML = `
+      <thead>
+        <tr>
+          <th>ID Centro Medico</th>
+          <th>Nombre Producto</th>
+          <th>Cantidad Producto</th>
+          <th>Capacidad Producto</th>
+          <th>Fecha Vencimiento</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${data.map(registro => `
+          <tr>
+            <td>${registro.id_centro_medico}</td>
+            <td>${registro.nombre_producto}</td>
+            <td>${registro.cantidad_producto}</td>
+            <td>${registro.capacidad_producto}</td>
+            <td>${registro.fecha_vencimiento}</td>
+          </tr>
+        `).join('')}
+      </tbody>
+    `;
+
+    searchInput.addEventListener('input', () => {
+      const searchText = searchInput.value.toLowerCase();
+      const rows = myTable.querySelectorAll('tbody tr');
+
+      rows.forEach(row => {
+        const idCentroMedico = row.querySelector('td:first-child').textContent.toLowerCase();
+        if (idCentroMedico.includes(searchText)) {
+          row.style.display = '';
+        } else {
+          row.style.display = 'none';
+        }
+      });
+    });
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const myTable = document.getElementById('TableInventario');
+const searchInput = document.getElementById('searchInput');
 
 fetch('http://3.22.74.121:3000/Inventario')
   .then(response => response.json())
@@ -20,3 +101,4 @@ fetch('http://3.22.74.121:3000/Inventario')
   })
   .catch(error => {
   });
+*/
