e854318d549012ca016779d92fd93d545582ae6f
jsantis <san21153@uvg.edu.gt>
2023-04-17 01:06:27 -0600

    Nuevo Historial POST

--- src/PACIENTES/Historial/historial.js
+++ src/PACIENTES/Historial/historial.js
@@ -63,3 +63,25 @@ fetch(`http://3.22.74.121:3000/Paciente/${dpi}`)
     .catch(error => {
       console.error(error);
     });
+
+    function enviarFormulario(formId) {
+      const form = document.getElementById(formId);
+      form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        const formData = new FormData(form);
+        const body = {};
+        formData.forEach((value, key) => body[key] = value);
+        const response = await fetch('http://3.22.74.121:3000/Historial', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(body)
+        });
+        const data = await response.json();
+        console.log(data);
+        form.reset();
+        alert('Los datos se han enviado correctamente');
+      });
+    }
+    
